2d06cfe94b914cbbcd71c29a779f0c51717b7f2a
Chris J W Walker <cjw@walker54.plus.com>
2018-11-22 21:07:48 +0000

    Refactor log processor

--- src/main/scala/com/cjwwdev/logging/encoders/JsonLoggingEncoding.scala
+++ src/main/scala/com/cjwwdev/logging/encoders/JsonLoggingEncoding.scala
@@ -37,9 +37,8 @@ class JsonLoggingEncoding extends EncoderBase[ILoggingEvent] {
 
   private def processLog(message: String): Option[(String, Int, String)]  = message match {
     case x if x.matches("""^(HEAD|GET|POST|PUT|PATCH|DELETE) request to (.*) returned a \d{3} and took \d+ms$""") =>
-      val digits = ("""\d+""".r findAllIn x).toList
-      val method = ("""(HEAD|GET|POST|PUT|PATCH|DELETE)""".r findAllIn x).toList.head
-      Some((method, digits.head.toInt, s"${digits.last}ms"))
+      val splitMessage = x.split(" ").toList
+      Some((splitMessage.head, splitMessage(splitMessage.length - 4).toInt, splitMessage.last))
     case _ => None
   }
 
